a40345ab3508713f930ad00074b3cea9e972072d
TimothyAlban <timothyalban@trentu.ca>
2022-02-05 00:14:52 -0500

    intial

---
+++ SQL/SQL/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace SQL
+{
+    class Item
+    {
+        //this needs to create the item object
+        public Item(int id)
+        {
+            string url = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=" + id.ToString();
+            string json ="";
+
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString(url);
+            }
+            Console.WriteLine("{0}", json);
+        }
+        // this needs to take the information and store it
+        public void Add()
+        {
+
+        }
+    }
+}
---
+++ SQL/SQL/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item(50);
+
+
+            Console.ReadLine();
+            /*
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "LAPTOP-B8Q8GRIK";
+                builder.UserID = "sa";
+                builder.Password = "admin123";
+                builder.InitialCatalog = "PracticeSQL";
+                Console.WriteLine("1");
+                Console.ReadLine();
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = builder.ConnectionString;
+
+                    conn.Open();
+
+
+                    Console.WriteLine("State: {0}", conn.State);  
+                    Console.WriteLine("ConnectionString: {0}",
+                        conn.ConnectionString);
+
+                }
+
+
+                Console.WriteLine("success");
+                Console.ReadLine();
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            */
+        }
+        private static string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code,
+            // you can retrieve it from a configuration file.
+            return "Server=(local);Integrated Security=SSPI;" +
+                "Initial Catalog=PracticeSQL";
+        }
+    }
+}
