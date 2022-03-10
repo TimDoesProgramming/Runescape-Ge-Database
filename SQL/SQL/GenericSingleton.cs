4c8e52605046317717498527d9634850ac34e182
TimothyAlban <timothyalban@trentu.ca>
2022-03-10 08:52:05 -0500

    Generic Singleton Class
    Added a singleton class for my dbmanager, though I probably wont use it.

---
+++ SQL/SQL/GenericSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    public sealed class GenericSingleton<T>
+    {
+        private static readonly GenericSingleton<T> instance = new GenericSingleton<T>();
+        T data = (T)default;
+        public T Data {
+            get { return data; }
+
+            set {
+                if (data.Equals((T)default))
+                    data = value;    
+            } 
+        }
+
+        static GenericSingleton()
+        {
+
+        }
+        private GenericSingleton()
+        {
+
+        }
+
+        public static GenericSingleton<T> Instance
+        {
+            get
+            {
+                return instance;
+            }
+                
+        }
+        public static
+
+    }
+}
--- SQL/SQL/dbManager.cs
+++ SQL/SQL/dbManager.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 namespace SQL
 {
-    class dbManager
+    public static class dbManager
     {
 
 
-        private void Connect()
+        public static void Connect()
         {
            
            try
@@ -20,8 +20,7 @@ namespace SQL
                builder.UserID = "sa";
                builder.Password = "admin123";
                builder.InitialCatalog = "PracticeSQL";
-               Console.WriteLine("1");
-               Console.ReadLine();
+
                using (SqlConnection conn = new SqlConnection())
                {
                    conn.ConnectionString = builder.ConnectionString;
@@ -47,8 +46,7 @@ namespace SQL
     
         private static string GetConnectionString()
         {
-            // To avoid storing the connection string in your code,
-            // you can retrieve it from a configuration file.
+
             return "Server=(local);Integrated Security=SSPI;" +
                 "Initial Catalog=PracticeSQL";
         }
